f5e9e163c30de8a86020b48603849bde561578cd
Johnny <cadam.technology@gmail.com>
2024-06-01 16:14:55 +0200

    Add files via upload

---
+++ Setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CTkKeyboard",
+    version='0.1',
+    author="Cadam",
+    description='Virtual Keyboard for CustomTkinter',
+    packages=find_packages(),
+    install_requires=['customtkinter'], 
+)
